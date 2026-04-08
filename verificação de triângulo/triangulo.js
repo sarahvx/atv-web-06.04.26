19702ef436b3b4cad3cf95cb712e198965287f5f
Sarah Ferreira <sarahfernandes2103@gmail.com>
2026-04-08 08:05:10 -0300

    Update triangulo.js

--- verificação de triângulo/triangulo.js
+++ verificação de triângulo/triangulo.js
@@ -1,5 +1,5 @@
 export function triangulo(num1, num2, num3){
-    if (num1 >= num2 + num3 || num2 >= num1 + num3 || num3 >= num1 + num2) return 'invalid'	
+    if (num1 >= num2 + num3 || num2 >= num1 + num3 || num3 >= num1 + num2) return 'none'	
     else if (num1 === num2 && num2 === num3 ) return 'equilateral';
     else if (num1 !== num2 && num2 !== num3 && num1 !== num3) return 'scalene'
     else
