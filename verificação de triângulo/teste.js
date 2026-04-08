bde1c109fc4d71e13630d56f196b99875e887c94
ifpb <ifpb@DSK20LB65-JP>
2026-04-08 08:02:09 -0300

    add

---
+++ verificação de triângulo/teste.js
@@ -0,0 +1,15 @@
+import { triangulo } from "./triangulo.js"
+
+console.log(triangulo(2, 2, 2));
+console.log(triangulo(10, 10, 10));
+console.log(triangulo(3, 4, 4));
+console.log(triangulo(4, 3, 4));
+console.log(triangulo(4, 4, 3));
+console.log(triangulo(10, 10, 2));
+console.log(triangulo(3, 4, 5));
+console.log(triangulo(10, 11, 12));
+console.log(triangulo(5, 4, 2));
+console.log(triangulo(0, 0, 0));
+console.log(triangulo(3, 4, -5));
+console.log(triangulo(1, 1, 3));
+console.log(triangulo(2, 4, 2));
--- verificação de triângulo/triangulo.js
+++ verificação de triângulo/triangulo.js
@@ -1,8 +1,8 @@
 export function triangulo(num1, num2, num3){
-    switch (num1, num2, num3) {
-        case num1 == num2 == num3:
-            return 'equilateral';
-    }
+    if (num1 >= num2 + num3 || num2 >= num1 + num3 || num3 >= num1 + num2) return 'invalid'	
+    else if (num1 === num2 && num2 === num3 ) return 'equilateral';
+    else if (num1 !== num2 && num2 !== num3 && num1 !== num3) return 'scalene'
+    else
+	return 'isosceles'
 }
 
-console.log(triangulo(2, 2, 2));
